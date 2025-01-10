efa7ae3d34a49e37dd786f077c00c0c47e540b42
LukeXH <luke.pa.hill@gmail.com>
2025-01-10 18:18:57 -0500

    CMake attempt

---
+++ src/cam_main.cpp
@@ -0,0 +1,16 @@
+#include <iomanip>
+#include <iostream>
+#include <memory>
+#include <thread>
+
+#include <libcamera/libcamera.h>
+
+using namespace libcamera;
+using namespace std::chrono_literals;
+
+int main()
+{
+    // Code to follow
+
+    return 0;
+}
