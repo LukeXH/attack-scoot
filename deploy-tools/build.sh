5927a91034289d8b92539fc19de5d3ec215dde9b
LukeXH <luke.pa.hill@gmail.com>
2025-01-10 19:03:37 -0500

    Update to build too to use meson

--- deploy-tools/build.sh
+++ deploy-tools/build.sh
@@ -1,9 +1,14 @@
+PKG_CONFIG_PATH=/usr/lib/arm-linux-gnueabihf/pkgconfig
+
 CURRDIR=$(dirname $(readlink -f "$0"))
 
 echo $CURRDIR
 
 cd $CURRDIR/..
-mkdir -p build
+meson build
 cd build
-cmake ..
-cmake --build .
+ninja
+# mkdir -p build
+# cd build
+# cmake ..
+# cmake --build .
