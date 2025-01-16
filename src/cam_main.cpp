f9a1233b565c0972f02f8127262354ac0df176b2
LukeXH <luke.pa.hill@gmail.com>
2025-01-15 21:05:30 -0500

    Image capture

--- src/cam_main.cpp
+++ src/cam_main.cpp
@@ -29,7 +29,6 @@ int main()
         cm->stop();
         return EXIT_FAILURE;
     }
-
     
     std::string cameraId = cameras[0]->id();
 
@@ -43,6 +42,50 @@ int main()
     // StreamConfiguration &streamConfig = config->at(0);
     StreamConfiguration &streamConfig = config->at(0);
     std::cout << "Default viewfinder configuration is: " << streamConfig.toString() << std::endl;
+
+    FrameBufferAllocator *allocator = new FrameBufferAllocator(camera);
+
+    for (StreamConfiguration &cfg : *config) {
+        int ret = allocator->allocate(cfg.stream());
+        if (ret < 0) {
+            std::cerr << "Can't allocate buffers" << std::endl;
+            return -ENOMEM;
+        }
+
+        size_t allocated = allocator->buffers(cfg.stream()).size();
+        std::cout << "Allocated " << allocated << " buffers for stream" << std::endl;
+    }
+
+    Stream *stream = streamConfig.stream();
+    const std::vector<std::unique_ptr<FrameBuffer>> &buffers = allocator->buffers(stream);
+    std::vector<std::unique_ptr<Request>> requests;
+
+    for (unsigned int i = 0; i < buffers.size(); ++i) {
+        std::unique_ptr<Request> request = camera->createRequest();
+        if (!request)
+        {
+            std::cerr << "Can't create request" << std::endl;
+            return -ENOMEM;
+        }
+
+        const std::unique_ptr<FrameBuffer> &buffer = buffers[i];
+        int ret = request->addBuffer(stream, buffer.get());
+        if (ret < 0)
+        {
+            std::cerr << "Can't set buffer for request"
+                << std::endl;
+            return ret;
+        }
+
+        requests.push_back(std::move(request));
+    }
+
+    camera->stop();
+    // allocator->free(stream);
+    // delete allocator;
+    camera->release();
+    camera.reset();
+    cm->stop();
 
     return 0;
 }
