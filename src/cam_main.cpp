3c15b097a35b96cc982e192516c7e4fce451d09d
LukeXH <luke.pa.hill@gmail.com>
2025-01-19 14:23:14 -0500

    Event handling example incorporated

--- src/cam_main.cpp
+++ src/cam_main.cpp
@@ -11,6 +11,37 @@ using namespace std::chrono_literals;
 // Global 
 static std::shared_ptr<Camera> camera;
 
+/* EVENT HANDLER */
+static void requestComplete(Request *request)
+{
+    // First check if the request has completed succesfully
+    // and wasn't cancelled
+    if (request->status() == Request::RequestCancelled)
+        return;
+
+    const std::map<const Stream *, FrameBuffer *> &buffers = request->buffers();
+
+    for (auto bufferPair : buffers) {
+        FrameBuffer *buffer = bufferPair.second;
+        const FrameMetadata &metadata = buffer->metadata();
+
+        std::cout << " seq: " << std::setw(6) << std::setfill('0') << metadata.sequence << " bytesused: ";
+
+        unsigned int nplane = 0;
+        for (const FrameMetadata::Plane &plane : metadata.planes())
+        {
+            std::cout << plane.bytesused;
+            if (++nplane < metadata.planes().size()) std::cout << "/";
+        }
+
+        std::cout << std::endl;
+    }
+
+    // request->reuse(Request::ReuseBuffers);
+    // camera->queueRequest(request);
+}
+
+/* MAIN */
 int main()
 {
     // Code to follow
@@ -87,6 +118,14 @@ int main()
 
         requests.push_back(std::move(request));
     }
+
+    // Event Handling
+    camera->requestCompleted.connect(requestComplete);
+    camera->start();
+    for (std::unique_ptr<Request> &request : requests)
+        camera->queueRequest(request.get());
+    // Wait for X milli-seconds to just see what we get
+    std::this_thread::sleep_for(1000ms);
 
     camera->stop();
     allocator->free(stream);
