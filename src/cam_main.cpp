562314182a683960d457ee6551ae5f44d5815ab4
LukeXH <luke.pa.hill@gmail.com>
2025-01-19 23:02:33 -0500

    Working on new class

--- src/cam_main.cpp
+++ src/cam_main.cpp
@@ -41,6 +41,51 @@ static void requestComplete(Request *request)
     camera->queueRequest(request);
 }
 
+/* EVENT HANDLER CLASS */
+class FrameHandler
+{
+private:
+    std::shared_ptr<Request> request_;
+public:
+    FrameHandler(/* args */) {}
+    ~FrameHandler() {}
+
+    void requestComplete(Request *request)
+    {
+        // First check if the request has completed succesfully
+        // and wasn't cancelled
+        if (request->status() == Request::RequestCancelled)
+            return;
+
+        const std::map<const Stream *, FrameBuffer *> &buffers = request->buffers();
+
+        for (auto bufferPair : buffers) {
+            FrameBuffer *buffer = bufferPair.second;
+            const FrameMetadata &metadata = buffer->metadata();
+
+            std::cout << " seq: " << std::setw(6) << std::setfill('0') << metadata.sequence << " bytesused: ";
+
+            unsigned int nplane = 0;
+            for (const FrameMetadata::Plane &plane : metadata.planes())
+            {
+                std::cout << plane.bytesused;
+                if (++nplane < metadata.planes().size()) std::cout << "/";
+            }
+
+            std::cout << std::endl;
+        }
+
+        request->reuse(Request::ReuseBuffers);
+        this->request_ = std::shared_ptr<Request>(request);
+        // camera->queueRequest(request);
+    }
+
+    void getFrames()
+    {
+        camera->queueRequest(this->request_.get());
+    }
+};
+
 /* MAIN */
 int main()
 {
