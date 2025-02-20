2dee76f298eed4c91d915fa91954bf644e77a989
LukeXH <luke.pa.hill@gmail.com>
2025-02-19 21:05:29 -0500

    Trying to cleanup a little

--- src/cam_main.cpp
+++ src/cam_main.cpp
@@ -47,12 +47,8 @@ static void requestComplete(Request *request)
             std::cout << "\nThere is at least one plane of length: " << plane.length << std::endl;
             void *memory = mmap(NULL, plane.length, PROT_READ | PROT_WRITE, MAP_SHARED, plane.fd.get(), 0);
             libcamera::Span<uint8_t> image_raw(static_cast<uint8_t *>(memory), plane.length);
-            Image8b image(480, 640*4, IM_8UC1, (uint8_t*)(image_raw.data())); // This formating works, don't know why
-            for (size_t i = 0; i < 5; i++)
-            {
-                std::cout << image_raw.data() << std::endl;
-            }
-
+            Image8b image(480, 640*4, IM_8UC1, (uint8_t*)(image_raw.data())); // This formating works, cuz we have 4 channels, RGBA
+            
             // Save as CSV
             std::ofstream im_file;
             std::string im_file_name = "/home/scoot/logs/imlog";
